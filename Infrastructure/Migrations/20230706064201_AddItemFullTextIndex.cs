1bbf973462a24c4555a7b08649ba7b50b5eec9ed
Руслан Ключенко <klyuchenkorus@gmail.com>
2023-07-06 10:18:19 +0300

    feat: fulltext index on items

---
+++ Infrastructure/Migrations/20230706064201_AddItemFullTextIndex.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemFullTextIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT CATALOG ftCatalog AS DEFAULT",
+                suppressTransaction: true );
+
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT INDEX ON Items(Name) KEY INDEX PK_Items",
+                suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT INDEX ON Items",
+                suppressTransaction: true);
+
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT CATALOG ftCatalog",
+                suppressTransaction: true);
+        }
+    }
+}
