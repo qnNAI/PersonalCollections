5a2e2358afd5f89d262f9198d784ad96acccfa00
Руслан Ключенко <klyuchenkorus@gmail.com>
2023-07-16 11:46:20 +0300

    feat: fulltext index on collections, comments

---
+++ Infrastructure/Migrations/20230716084213_AddFulltextIndexCollection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFulltextIndexCollection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT INDEX ON Collections(Name) KEY INDEX PK_Collections WITH CHANGE_TRACKING AUTO, STOPLIST OFF",
+                suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT INDEX ON Collections",
+                suppressTransaction: true);
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20230716084448_AddFulltextIndexComments.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFulltextIndexComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "CREATE FULLTEXT INDEX ON Comments(Text) KEY INDEX PK_Comments WITH CHANGE_TRACKING AUTO, STOPLIST OFF",
+                suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "DROP FULLTEXT INDEX ON Comments",
+                suppressTransaction: true);
+        }
+    }
+}
